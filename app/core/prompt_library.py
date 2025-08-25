71e9621447f601c2492a3bf887e31644ab0f9529
m.nithish kumar <you@example.com>
2025-08-25 13:11:03 +0530

    fifth commit

--- app/core/prompt_library.py
+++ app/core/prompt_library.py
@@ -1,10 +1,10 @@
-PROMPT_TEMPLATES1={
-    "prompt1":"""
-            As you are an analyzer agent extract seat_no,passener_name,location datetime from the input
-            And stricly follow this one if user give the date and time convert into the these format '2025-06-10 12:00:00'
-            you don't manaully put the time and date format take user time and dataformat whatever mentioned by the user input 
-    """
-}
+# PROMPT_TEMPLATES1={
+#     "prompt1":"""
+#             As you are an analyzer agent extract seat_no,passener_name,location datetime from the input
+#             And stricly follow this one if user give the date and time convert into the these format '2025-06-10 12:00:00'
+#             you don't manaully put the time and date format take user time and dataformat whatever mentioned by the user input 
+#     """
+# }
 
 PROMPT_TEMPLATES2={
     "prompt2":"""
